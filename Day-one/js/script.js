41c2d695bd707e77c944c7bad312d248266e6ea9
Aksume <84611765+Aksume@users.noreply.github.com>
2021-11-06 15:55:45 -0500

    first commit

---
+++ Day-one/js/script.js
@@ -0,0 +1,21 @@
+// console.log("javascript");
+console.log("Tsion");
+console.log("Apple");
+
+// Single line comments start with //.
+
+// Any text between // and the end of the line will be ignored by JavaScript (will not be executed).
+
+// This example uses a single-line comment before each code line:
+
+// *******************************
+// Multi-line comments start with /* and end with */.
+
+// Any text between /* and */ will be ignored by JavaScript.
+
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+in my web page:
+*/
