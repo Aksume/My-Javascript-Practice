1345d890429ee05217a3b45cf8638d7eaec79e59
Aksume <84611765+Aksume@users.noreply.github.com>
2021-11-07 19:57:18 -0600

    (Day-2

---
+++ Day-2/js/script.js
@@ -0,0 +1,63 @@
+console.log("Tsion");
+console.log("My favorite thing is coding");
+
+let letters = "Supercalifragilisticexpialidocious";
+console.log(letters.length);
+
+let x = 20;
+console.log(x);
+
+let bigNum = 2014n;
+console.log(bigNum);
+
+// *******************************************
+
+let adj1 = "fancy";
+// Assign an adjective
+let adj2 = "mysterious";
+// Assign an adjective
+let adj3 = "immens";
+// Assign an adjective
+let adj4 = "drab";
+// Assign an adjective
+let adj5 = "itchy";
+// Assign an adjective
+let adj6 = "shy";
+// Assign an adverb
+let adverb = "hoplessly";
+// Assign a color
+let color = "pous";
+// Assign a noun
+let noun1 = "art";
+// Assign a noun
+let noun2 = "umberella";
+// Assign a noun
+let noun3 = "button";
+// Assign a noun
+let noun4 = "cup";
+// Assign a noun
+let noun5 = "potato";
+// Assign a noun
+let noun6 = "moon";
+// Assign a number
+let num = "9";
+// Assign a plural noun
+let pnoun1 = "cows";
+
+// Open your console to see the results!
+// SPOILER ALERT: KEEP SCROLLING TO VIEW THE REST OF THE CODE
+
+//Using template literals to plug in the values
+let story = `It has often been said that a dog is a man's best ${noun1}. Dogs are very ${adj1} and can be taught many ${adj2}
+tricks. A dog can be trained to carry a ${noun2} in his mouth.
+If you throw his ${noun3}, he will run and fetch it. Dogs
+will also bark ${adverb} if someone tries to break into your
+${noun4} during the night. One of the most popular canine pets
+today is the ${noun5} Spaniel. Spaniels have curly ${color}
+coats and ${adj3} ears. They also have very ${adj4}
+dispositions and live to be ${num} years old. Other popular dogs
+are ${adj5} Terriers, German ${pnoun1}, and the
+${adj6} Poodle. Every ${noun6} should have a loyal dog.`;
+
+//Console log the completed story
+console.log(story);
